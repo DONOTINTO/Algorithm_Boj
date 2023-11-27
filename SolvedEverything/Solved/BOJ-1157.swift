fa856f670594598b47eff2b630cd9511018d6550
DONOTINTO <goods99j@gmail.com>
2023-11-27 17:54:26 +0900

    =update=BOJ-1157

---
+++ SolvedEverything/Solved/BOJ-1157.swift
@@ -0,0 +1,27 @@
+/*
+ [Boj-1157]
+ 알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램을 작성하시오. 단, 대문자와 소문자를 구분하지 않는다.
+ 
+ [INPUT]
+ 첫째 줄에 알파벳 대소문자로 이루어진 단어가 주어진다. 주어지는 단어의 길이는 1,000,000을 넘지 않는다.
+ 
+ [OUTPUT]
+ 첫째 줄에 이 단어에서 가장 많이 사용된 알파벳을 대문자로 출력한다. 단, 가장 많이 사용된 알파벳이 여러 개 존재하는 경우에는 ?를 출력한다.
+ */
+
+let wording: String = readLine()!
+var alphabet: [Int] = Array(repeating: 0, count: 26)
+func makeUppercase(word: Character) -> Character { Character(word.uppercased()) }
+
+wording.forEach {
+    let index = Int(makeUppercase(word: $0).asciiValue!) - 65
+    alphabet[index] += 1
+}
+
+let maxElement = alphabet.max()!
+
+if alphabet.filter({ $0 == maxElement }).count > 1 {
+    print("?")
+} else {
+    print(UnicodeScalar((alphabet.firstIndex(of: maxElement))! + 65)!)
+}
